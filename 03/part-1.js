38e011cf440d49e4ef7fc261571b6288a02b693c
Tony Ko <tko@qmo.io>
2019-12-05 17:16:30 -0500

    Set up placeholder functions to intersects

--- 03/part-1.js
+++ 03/part-1.js
@@ -45,29 +45,36 @@ export function codeToVector(routeCode) {
 }
 
 // local vectors describe a direction in X,Y from 0,0 (R1000 = X: 1000, Y: 0)
-export function getLocalVectors(codeSet) {
-  return codeSet.map((code) => codeToVector(code));
+export function getLocalVectors(codeSets) {
+  return codeSets.map((codeSet) => codeSet.map((code) => codeToVector(code)));
 }
 
 // absolute vectors describe a coord in X, Y
-export function getAbsoluteVectors(vectorSet, startCoords) {
-  return vectorSet.reduce(
-    (rslt, vector, idx) => {
+export function getAbsoluteCoords(vectorSets, startCoords) {
+  return vectorSets.map((vectorSet) => {
+    return vectorSet.reduce((rslt, vector, idx) => {
       rslt.push(rslt[idx].add(vector));
       return rslt;
-    },
-    [startCoords]
-  );
+    }, [startCoords]);
+  });
+}
+
+export function getIntersects(coordSets) {
+  // placeholder
+}
+
+export function getClosestIntersect(intersects) {
+  // placeholder
 }
 
 export function getSolution(err, data) {
   if (err) throw err;
   const codeSets = getInputArr(data);
-  const vectorSets = codeSets.map((codeSet) => getLocalVectors(codeSet));
-  const coordSets = vectorSets.map((vectorSet) => getAbsoluteVectors(vectorSet, new Vector(0, 0)));
-  // const intersects = vectorSets.map((coordSet, coordSetIdx) => getIntersects(coordSet, coordSets, coorSetIdx));
-  console.log(coordSets);
-  console.log('[03 - Part 1] Solution:', '');
+  const vectorSets = getLocalVectors(codeSets);
+  const coordSets = getAbsoluteCoords(vectorSets, new Vector(0, 0));
+  const intersects = getIntersects(coordSets);
+  const closestIntersect = getClosestIntersects(intersects);
+  console.log('[03 - Part 1] Solution:', closestIntersect);
 }
 
 readFile(join(__dirname, 'input.txt'), 'utf8', getSolution);
